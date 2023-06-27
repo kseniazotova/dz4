d62b7c1b312d06b8c830740fa3cd19dfdcfae9db
ksenia <zotova508@mail.ru>
2023-06-27 12:24:18 +0300

    Добавила решение к задаче 27 и 29

---
+++ dz4/27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 452 -> 11
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()!);
+int b = Math.Abs ( Method1(a));
+Console.WriteLine($"Cумма цифр в числе {а} = {b}");
+
+int Method1 (int chislo)
+{
+int sum = 0;
+while (chislo != 0)
+{
+sum += chislo % 10;
+chislo /= 10;
+}
+return sum;
+}
---
+++ dz4/29/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+int items = 8;
+int[] array = new int[items];
+FillArray (array);
+PrintArray (array);
+void FillArray(int [] arr)
+{
+Random rnd = new Random();
+for (int i = 0; i < arr. Length; i=i+1)
+{
+arr[i] = rnd.Next(100);
+}}
+void PrintArray(int[ ] arr)
+{
+for (int i = 0; i < array. Length; i=i+1)
+{
+Console.Write(array[i] + " ");
+}
+}
+
